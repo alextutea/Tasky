c26b85dd82fbbd41b4e449f5ececdf8dcac343d6
Alex Tutea <alex.tutea@gmail.com>
2020-02-20 23:49:58 +0100

    Basic cards

---
+++ tasky-front/src/components/Task.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card
+    max-width="400"
+    class="mx-auto"
+  >
+  <v-container>
+      <v-row dense>
+        <v-col cols="12">
+          <v-card
+            color="#385F73"
+            dark
+          >
+            <v-card-title class="headline">{{cardTitle}}</v-card-title>
+
+            <v-card-subtitle>{{cardContent}}</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text>{{cardAuthor}}</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    cardTitle: {
+        type: String,
+        required: true
+    },
+    cardContent:{
+        type: String,
+        required: true
+    },
+    cardAuthor:{
+        type: String,
+        required: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ tasky-front/src/components/TaskList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="tasklist">
+    <h3>Task List</h3>
+    <Task v-for="n in howMany" :key="n" card-title="Some Title" card-author="Alex" card-content="Some content"/>
+  </div>
+</template>
+
+<script>
+
+import Task from "./Task"
+
+export default {
+  name: 'TaskList',
+  components:{
+      Task
+  },
+  props: {
+    howMany: Number
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
