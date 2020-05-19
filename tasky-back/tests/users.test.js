6f63d9cca29a796f73f0d4c698587f8bb52cc4e0
Alex Tutea <alex.tutea@gmail.com>
2020-05-19 02:09:32 +0200

    Fixed integration with github actions

--- tasky-back/tests/users.test.js
+++ tasky-back/tests/users.test.js
@@ -36,7 +36,7 @@ describe('Register', () => {
         const email = "test-user1@email.com";
         const password = "ASDqwe123";
         const username = "test-user1";
-        await axios.post(`http://tasky-back:${port}/auth/register`, {email, password}).then(
+        await axios.post(`http://tasky-back:${port}/auth/register`, {email, password, username}).then(
             response => {
                 expect(response.data.username).toBe(username);
                 expect(response.data.email).toBe(email);
